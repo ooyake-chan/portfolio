11e83b7f4aad2e985b7bb07aa85ced76fb04f852
muramatsu <tendouzi1105@gmail.com>
2021-01-17 10:19:15 +0900

    chore:画像の最適化、SEOの設定のためのプラグインを追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,25 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata:{
+    title:`mikisPortfolio`,
+    description:`村松美紀のポートフォリオサイトです`,
+    lang:`ja`,
+    siteUrl:`https://ooyakechan.shop/portfolio`,
+    locale:`ja_JP`,
+    fbappid:`xxxxx`,
+  },
+
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name : `images`,
+        path : `${__dirname}/src/images`
+      },
+    },
+    `gatsby-plugin-react-helmet`
+  ],
 }
